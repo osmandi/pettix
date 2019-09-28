85a0778cb8e76dcb4b64761f3abf41bedcd23264
Julio Noguera <noguerajulio24@gmail.com>
2019-09-28 13:40:40 -0500

    lasted

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -8,22 +8,7 @@ module Constants
   }
 
   CITY = {
-      Afganistán: "Kabul",
-      Albania: "Tirana",
-      Alemania: "Berlín",
-      Andorra: "Andorra la Vieja",
-      Angola: "Luanda",
-      Arabia "Saudita: Riad",
-      Argelia: "Argel",
-      Argentina: "Buenos Aires",
-      Armenia: "Ereván",
-      Australia: "Camberra",
-      Austria: "Viena",
-      Azerbaiyán: "Bakú",
-      Bahamas: "Nasáu",
-      Bangladés: "Daca",
-      Barbados: "Bridgetown",
-      Colombia: "Bogotá",
+      colombia: "Bogotá",
   }
 
 end
